b314fb461f71bcf01a0cebb2613158b02604de6c
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-13 23:09:57 -0300

    fix: corrigido erro ao iniciar use state do array tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,30 @@ import './App.css'
 import TaskItem from './components/TaskItem'
 
 function App() {
-  const [tasks, setTasks] = useState([{
-    titulo: "teste1",
-    descricao: "descricao1"
-  }, {
-    titulo: "teste2",
-    descricao: "descricao2"
-  }])
+  const [tasks, setTasks] = useState([
+    {titulo: "título 1", descricao: "descricao 1"},
+    {titulo: "título 2", descricao: "descricao 2"}
+  ])
+
+  const [inputTitulo, setInputTitulo] = useState('')
+  const [inputDescricao, setInputDescricao] = useState('')
+
+  const handleBotaoAdicionar = () => {
+    setTasks(prevTasks => [...prevTasks, {titulo: inputTitulo, descricao: inputDescricao}])
+  }
 
   return (
     <>
       <div>
-        <input type='text' placeholder='Título da tarefa'/>
+        <input type='text' placeholder='Título da tarefa' onChange={event => setInputTitulo(event.target.value)}/>
         <br/>
-        <input type='text' placeholder='Descricao da tarefa'/>
+        <input type='text' placeholder='Descricao da tarefa' onChange={event => setInputDescricao(event.target.value)}/>
         <br/>
-        <button>Adicionar</button>
+        <button onClick={() => handleBotaoAdicionar()}>Adicionar</button>
       </div>
 
       <div>
-        {tasks.map(task => (
+        {tasks?.map(task => (
           <TaskItem titulo={task.titulo} descricao={task.descricao}/>
         ))}
         

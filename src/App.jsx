0eda35ede2c46618f77f28025fc5b3f425bd408c
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-16 22:42:34 -0300

    feat: alterando router

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import CadastrarPage from './pages/cadastrar';
+import RootPage from './pages/root';
+
+function App() {
+  return (
+    <Router>
+        <Switch>
+            <Route path=''>
+
+            </Route>
+        </Switch>
+    </Router>
+  )
+}
+
+export default App;

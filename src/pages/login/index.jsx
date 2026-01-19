1c6ec64736beb55a9656d1a41b66cfa7f3e382d7
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-19 12:23:23 -0300

    feat: tela login ajustado

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <div>
-        <div>
-            <label htmlFor="email">Email: </label>
+    <div class="container d-flex justify-content-center">
+      <div class="column">
+        <div class="d-flex justify-content-between p-1">
+            <label htmlFor="email" class="m-2">Email: </label>
             <input type='email' name='email' placeholder='Informe seu e-mail' />
         </div>
-        <div>
-            <label htmlFor="senha">Senha: </label>
+        <div class="d-flex justify-content-between p-1">
+            <label htmlFor="senha" class="m-2">Senha: </label>
             <input type='password' name='senha' placeholder='Informe sua senha' />
         </div>
-        <Link to="/inicial">Entrar</Link>
+        <div class="d-flex justify-content-center">
+          <Link class="btn btn-primary" to="/inicial">Entrar</Link>
+        </div>
       </div>
     </div>
   )

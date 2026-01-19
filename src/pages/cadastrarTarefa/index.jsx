57e0261d6d3b43375a1c5019c8cc8d472d0c1b27
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-19 12:50:33 -0300

    feat: ajustado tela de cadastrar tarefa

--- src/pages/cadastrarTarefa/index.jsx
+++ src/pages/cadastrarTarefa/index.jsx
@@ -31,29 +31,31 @@ function CadastrarPage() {
   }
 
   return (
-    <>
-      <div>
-        <div>
-          <label>Título: </label>
+    <div class="container d-flex justify-content-center pt-5">
+      <div class="column">
+        <div class="d-flex justify-content-between p-1">
+          <label class="m-1">Título: </label>
           <input type='text' placeholder='Título da tarefa' onChange={event => setInputTitulo(event.target.value)} value={inputTitulo} name='titulo'/>
         </div>
-        <div>
-          <label>Descrição: </label>
+        <div class="d-flex justify-content-between p-1">
+          <label class="m-1">Descrição: </label>
           <input type='text' placeholder='Descricao da tarefa' onChange={event => setInputDescricao(event.target.value)} value={inputDescricao} name='descricao'/>
         </div>
-        <div>
-          <label>Categoria: </label>
+        <div class="d-flex justify-content-between p-1">
+          <label class="m-1">Categoria: </label>
           <select name="categoria" id="categoria" onChange={event => setInputCategoria(event.target.value)}>
             {categorias?.map(categoria => (
               <option value={categoria}>{categoria}</option>
             ))}
           </select>
         </div>
-        <button onClick={() => handleBotaoAdicionar()}>Adicionar</button>
-        <button onClick={() => limparCampos()}>Limpar</button>
-        <Link to="/">Voltar</Link>
+        <div class="d-flex justify-content-between p-1">
+          <button class="btn btn-primary m-2" onClick={() => handleBotaoAdicionar()}>Adicionar</button>
+          <button class="btn btn-danger m-2" onClick={() => limparCampos()}>Limpar</button>
+          <Link class="btn btn-danger m-2" to="/inicial">Voltar</Link>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 

a4737b855fbcec89358984a82ff4d45247fb2291
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-19 12:34:46 -0300

    feat: teka de cadastrar usuário ajustado

--- src/pages/cadastrarUsuario/index.jsx
+++ src/pages/cadastrarUsuario/index.jsx
@@ -3,24 +3,26 @@ import { Link } from 'react-router-dom';
 
 const CadastrarUsuario = () => {
   return (
-    <div>
-          <div>
-            <div>
-                <label htmlFor="email">Email: </label>
-                <input type='email' name='email' placeholder='Informe o e-mail' />
-            </div>
-            <div>
-                <label htmlFor="senha">Senha: </label>
-                <input type='password' name='senha' placeholder='Informe a senha' />
-            </div>
-            <div>
-                <label htmlFor="confirmarSenha">Senha: </label>
-                <input type='password' name='confirmarSenha' placeholder='Redigite a sua senha' />
-            </div>
-            <Link to="/">Cadastrar</Link>
-            <Link to="/">Voltar</Link>
-          </div>
+    <div class="container d-flex justify-content-center">
+      <div class="column">
+        <div class="d-flex justify-content-between p-1">
+            <label class="m-1" htmlFor="email">Email: </label>
+            <input type='email' name='email' placeholder='Informe o e-mail' />
         </div>
+        <div class="d-flex justify-content-between p-1">
+            <label class="m-1" htmlFor="senha">Senha: </label>
+            <input type='password' name='senha' placeholder='Informe a senha' />
+        </div>
+        <div class="d-flex justify-content-between p-1">
+            <label class="m-1" htmlFor="confirmarSenha">Senha: </label>
+            <input type='password' name='confirmarSenha' placeholder='Redigite a sua senha' />
+        </div>
+        <div class="d-flex justify-content-center p-1">
+          <Link class="btn btn-primary m-1" to="/">Cadastrar</Link>
+          <Link class="btn btn-primary m-1" to="/">Voltar</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 

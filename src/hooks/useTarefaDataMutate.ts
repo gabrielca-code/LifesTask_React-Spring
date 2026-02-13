60e4d6983f8e4537c8fd957e6b68c2595ca2093a
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-12 22:32:54 -0300

    feat: POST Tarefa adicionado ao projeto

---
+++ src/hooks/useTarefaDataMutate.ts
@@ -0,0 +1,23 @@
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import axios, { AxiosPromise } from "axios"
+import { TarefaData } from "../interface/TarefaData"
+
+const postData = async (data: TarefaData): AxiosPromise<any> => {
+    const response = axios.post("http://localhost:8080/tarefa", data)
+    return response
+}
+
+export function useTarefaData() {
+    const queryClient = useQueryClient()
+    const mutate = useMutation({
+        mutationFn: postData,
+        retry: 2,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['tarefa-data']})
+        }
+    })
+
+    console.log(mutate)
+
+    return mutate
+}
